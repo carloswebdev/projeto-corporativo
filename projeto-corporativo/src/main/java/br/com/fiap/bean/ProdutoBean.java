b4a551b259c047ffd2d6ef2c780179111dd981c4
unknown <rm48014@L2201MICRO22.fiap.com.br>
2015-12-09 22:08:10 -0200

    -

---
+++ projeto-corporativo/src/main/java/br/com/fiap/bean/ProdutoBean.java
@@ -0,0 +1,58 @@
+package br.com.fiap.bean;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.inject.Inject;
+
+import br.com.fiap.dao.ProdutoDao;
+import br.com.fiap.model.Produto;
+
+@ManagedBean
+@ViewScoped
+public class ProdutoBean {
+
+	@Inject
+	private ProdutoDao produtoDao;
+
+	private Produto produto;
+	
+	@PostConstruct
+	public void init() {
+		produto = new Produto();
+	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
+	
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	public void add() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		FacesMessage msg = new FacesMessage();
+
+		try {
+			produtoDao.adiciona(produto);
+			msg.setDetail("Incluido com Sucesso");
+			msg.setSeverity(FacesMessage.SEVERITY_INFO);
+		} catch (Exception e) {
+			msg.setDetail(e.getMessage());
+			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+		}
+
+		context.addMessage(null, msg);
+	}
+
+	public List<Produto> listaTodos() {
+		return produtoDao.listaTodos();
+	}
+}
