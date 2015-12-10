ab1483f5b1b2d6b14312a90056a72c4e18dedaef
unknown <RM49649@L2201MICRO25.fiap.com.br>
2015-12-09 22:26:51 -0200

    Acrescentada a tabela login.

--- DB/db_fiap_ecommerce.sql
+++ DB/db_fiap_ecommerce.sql
@@ -12,9 +12,24 @@ DROP SCHEMA IF EXISTS `fiap_ecommerce` ;
 -- -----------------------------------------------------
 -- Schema fiap_ecommerce
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `fiap_ecommerce` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+CREATE SCHEMA IF NOT EXISTS `fiap_ecommerce` DEFAULT CHARACTER SET utf8 ;
 SHOW WARNINGS;
 USE `fiap_ecommerce` ;
+
+-- -----------------------------------------------------
+-- Table `blacklist`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `blacklist` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `blacklist` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `cpf` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+SHOW WARNINGS;
 
 -- -----------------------------------------------------
 -- Table `cliente`
@@ -23,19 +38,20 @@ DROP TABLE IF EXISTS `cliente` ;
 
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `cliente` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(45) NULL,
-  `senha` VARCHAR(45) NULL,
-  `endereco_cep` VARCHAR(500) NULL,
-  `cpf` VARCHAR(45) NULL,
-  `nome` VARCHAR(45) NULL,
-  `nome_mae` VARCHAR(45) NULL,
-  `telefone` VARCHAR(45) NULL,
-  `profissao_cbo` VARCHAR(45) NULL,
-  `salario` DECIMAL(10) NULL,
-  `data_nascimento` DATETIME NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `senha` VARCHAR(45) NULL DEFAULT NULL,
+  `endereco_cep` VARCHAR(500) NULL DEFAULT NULL,
+  `cpf` VARCHAR(45) NULL DEFAULT NULL,
+  `nome` VARCHAR(45) NULL DEFAULT NULL,
+  `nome_mae` VARCHAR(45) NULL DEFAULT NULL,
+  `telefone` VARCHAR(45) NULL DEFAULT NULL,
+  `profissao_cbo` VARCHAR(45) NULL DEFAULT NULL,
+  `salario` DECIMAL(10,0) NULL DEFAULT NULL,
+  `data_nascimento` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 
@@ -46,10 +62,11 @@ DROP TABLE IF EXISTS `status` ;
 
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `status` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `descricao` VARCHAR(45) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 
@@ -60,10 +77,10 @@ DROP TABLE IF EXISTS `pedido` ;
 
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `pedido` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `cliente_id` INT NOT NULL,
-  `status_id` INT NOT NULL,
-  `data_criacao` DATETIME NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `cliente_id` INT(11) NOT NULL,
+  `status_id` INT(11) NOT NULL,
+  `data_criacao` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_Pedido_cliente1`
     FOREIGN KEY (`cliente_id`)
@@ -75,7 +92,8 @@ CREATE TABLE IF NOT EXISTS `pedido` (
     REFERENCES `status` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 CREATE INDEX `fk_Pedido_cliente1_idx` ON `pedido` (`cliente_id` ASC);
@@ -86,44 +104,43 @@ CREATE INDEX `fk_pedido_status1_idx` ON `pedido` (`status_id` ASC);
 SHOW WARNINGS;
 
 -- -----------------------------------------------------
+-- Table `boleto`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `boleto` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `boleto` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `pedido_id` INT(11) NOT NULL,
+  `codigo_banco` INT(11) NULL DEFAULT NULL,
+  `vencimento` DATETIME NULL DEFAULT NULL,
+  `data_documento` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_boleto_pedido1`
+    FOREIGN KEY (`pedido_id`)
+    REFERENCES `pedido` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+SHOW WARNINGS;
+CREATE INDEX `fk_boleto_pedido1_idx` ON `boleto` (`pedido_id` ASC);
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
 -- Table `categoria_produto`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `categoria_produto` ;
 
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `categoria_produto` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-SHOW WARNINGS;
-
--- -----------------------------------------------------
--- Table `produto`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `produto` ;
-
-SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `produto` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `categoria_produto_id` INT NOT NULL,
-  `nome` VARCHAR(100) NULL,
-  `breve_descricao` VARCHAR(500) NULL,
-  `descricao` VARCHAR(2000) NULL,
-  `preco` DECIMAL(10) NULL,
-  `fabricante` VARCHAR(100) NULL,
-  `url_imagem_produto` VARCHAR(1000) NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `fk_produto_categoria_produto`
-    FOREIGN KEY (`categoria_produto_id`)
-    REFERENCES `categoria_produto` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-SHOW WARNINGS;
-CREATE INDEX `fk_produto_categoria_produto_idx` ON `produto` (`categoria_produto_id` ASC);
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 
@@ -134,62 +151,40 @@ DROP TABLE IF EXISTS `estoque` ;
 
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `estoque` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `endereco` VARCHAR(45) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `endereco` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `itens_estoque`
+-- Table `produto`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `itens_estoque` ;
+DROP TABLE IF EXISTS `produto` ;
 
 SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `itens_estoque` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `produto_id` INT NOT NULL,
-  `estoque_id` INT NOT NULL,
-  `quantidade` INT NULL,
+CREATE TABLE IF NOT EXISTS `produto` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `categoria_produto_id` INT(11) NOT NULL,
+  `nome` VARCHAR(100) NULL DEFAULT NULL,
+  `breve_descricao` VARCHAR(500) NULL DEFAULT NULL,
+  `descricao` VARCHAR(2000) NULL DEFAULT NULL,
+  `preco` DECIMAL(10,0) NULL DEFAULT NULL,
+  `fabricante` VARCHAR(100) NULL DEFAULT NULL,
+  `url_imagem_produto` VARCHAR(1000) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_itens_estoque_produto1`
-    FOREIGN KEY (`produto_id`)
-    REFERENCES `produto` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_itens_estoque_estoque1`
-    FOREIGN KEY (`estoque_id`)
-    REFERENCES `estoque` (`id`)
+  CONSTRAINT `fk_produto_categoria_produto`
+    FOREIGN KEY (`categoria_produto_id`)
+    REFERENCES `categoria_produto` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
-CREATE INDEX `fk_itens_estoque_produto1_idx` ON `itens_estoque` (`produto_id` ASC);
-
-SHOW WARNINGS;
-CREATE INDEX `fk_itens_estoque_estoque1_idx` ON `itens_estoque` (`estoque_id` ASC);
-
-SHOW WARNINGS;
-
--- -----------------------------------------------------
--- Table `politica_credito`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `politica_credito` ;
-
-SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `politica_credito` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `valor_produto` DECIMAL(10) NULL,
-  `valor_produto_comparacao` VARCHAR(5) NULL,
-  `cliente_base` TINYINT(1) NULL,
-  `blacklist` TINYINT(1) NULL,
-  `divida_com_empresa` TINYINT(1) NULL,
-  `spc` TINYINT(1) NULL,
-  `resultado` TINYINT(1) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE INDEX `fk_produto_categoria_produto_idx` ON `produto` (`categoria_produto_id` ASC);
 
 SHOW WARNINGS;
 
@@ -200,11 +195,11 @@ DROP TABLE IF EXISTS `item_pedido` ;
 
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `item_pedido` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `pedido_id` INT NOT NULL,
-  `produto_id` INT NOT NULL,
-  `quantidade` INT NULL,
-  `preco` DECIMAL(10) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `pedido_id` INT(11) NOT NULL,
+  `produto_id` INT(11) NOT NULL,
+  `quantidade` INT(11) NULL DEFAULT NULL,
+  `preco` DECIMAL(10,0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_item_pedido_pedido1`
     FOREIGN KEY (`pedido_id`)
@@ -216,7 +211,8 @@ CREATE TABLE IF NOT EXISTS `item_pedido` (
     REFERENCES `produto` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 CREATE INDEX `fk_item_pedido_pedido1_idx` ON `item_pedido` (`pedido_id` ASC);
@@ -227,27 +223,60 @@ CREATE INDEX `fk_item_pedido_produto1_idx` ON `item_pedido` (`produto_id` ASC);
 SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `boleto`
+-- Table `itens_estoque`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `boleto` ;
+DROP TABLE IF EXISTS `itens_estoque` ;
 
 SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `boleto` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `pedido_id` INT NOT NULL,
-  `codigo_banco` INT NULL,
-  `vencimento` DATETIME NULL,
-  `data_documento` DATETIME NULL,
+CREATE TABLE IF NOT EXISTS `itens_estoque` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `produto_id` INT(11) NOT NULL,
+  `estoque_id` INT(11) NOT NULL,
+  `quantidade` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_boleto_pedido1`
-    FOREIGN KEY (`pedido_id`)
-    REFERENCES `pedido` (`id`)
+  CONSTRAINT `fk_itens_estoque_estoque1`
+    FOREIGN KEY (`estoque_id`)
+    REFERENCES `estoque` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_itens_estoque_produto1`
+    FOREIGN KEY (`produto_id`)
+    REFERENCES `produto` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
-CREATE INDEX `fk_boleto_pedido1_idx` ON `boleto` (`pedido_id` ASC);
+CREATE INDEX `fk_itens_estoque_produto1_idx` ON `itens_estoque` (`produto_id` ASC);
+
+SHOW WARNINGS;
+CREATE INDEX `fk_itens_estoque_estoque1_idx` ON `itens_estoque` (`estoque_id` ASC);
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `login`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `login` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `usuario` VARCHAR(45) NULL DEFAULT NULL,
+  `senha` VARCHAR(45) NULL DEFAULT NULL,
+  `cliente_id` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_login_cliente1`
+    FOREIGN KEY (`cliente_id`)
+    REFERENCES `cliente` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+SHOW WARNINGS;
+CREATE INDEX `fk_login_cliente1_idx` ON `login` (`cliente_id` ASC);
 
 SHOW WARNINGS;
 
@@ -258,18 +287,40 @@ DROP TABLE IF EXISTS `nota_fiscal` ;
 
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `nota_fiscal` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `pedido_id` INT NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `pedido_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_nota_fiscal_pedido1`
     FOREIGN KEY (`pedido_id`)
     REFERENCES `pedido` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 CREATE INDEX `fk_nota_fiscal_pedido1_idx` ON `nota_fiscal` (`pedido_id` ASC);
+
+SHOW WARNINGS;
+
+-- -----------------------------------------------------
+-- Table `politica_credito`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `politica_credito` ;
+
+SHOW WARNINGS;
+CREATE TABLE IF NOT EXISTS `politica_credito` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `valor_produto` DECIMAL(10,0) NULL DEFAULT NULL,
+  `valor_produto_comparacao` VARCHAR(5) NULL DEFAULT NULL,
+  `cliente_base` TINYINT(1) NULL DEFAULT NULL,
+  `blacklist` TINYINT(1) NULL DEFAULT NULL,
+  `divida_com_empresa` TINYINT(1) NULL DEFAULT NULL,
+  `spc` TINYINT(1) NULL DEFAULT NULL,
+  `resultado` TINYINT(1) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 
@@ -280,10 +331,10 @@ DROP TABLE IF EXISTS `rastreabilidade` ;
 
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `rastreabilidade` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `pedido_id` INT NOT NULL,
-  `status_id` INT NOT NULL,
-  `data` DATETIME NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `pedido_id` INT(11) NOT NULL,
+  `status_id` INT(11) NOT NULL,
+  `data` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_rastreabilidade_pedido1`
     FOREIGN KEY (`pedido_id`)
@@ -295,7 +346,8 @@ CREATE TABLE IF NOT EXISTS `rastreabilidade` (
     REFERENCES `status` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 CREATE INDEX `fk_rastreabilidade_pedido1_idx` ON `rastreabilidade` (`pedido_id` ASC);
@@ -306,30 +358,17 @@ CREATE INDEX `fk_rastreabilidade_status1_idx` ON `rastreabilidade` (`status_id` 
 SHOW WARNINGS;
 
 -- -----------------------------------------------------
--- Table `blacklist`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `blacklist` ;
-
-SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `blacklist` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `cpf` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-SHOW WARNINGS;
-
--- -----------------------------------------------------
 -- Table `spc`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `spc` ;
 
 SHOW WARNINGS;
 CREATE TABLE IF NOT EXISTS `spc` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `cpf` VARCHAR(45) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `cpf` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 SHOW WARNINGS;
 
